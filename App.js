fd37d14cc3c9d5656dbadfb9e36c3021a6b5862a
Mankgaba23 <Thabilemankgaba@gmail.com>
2022-02-23 14:49:07 +0200

    signup page added

--- App.js
+++ App.js
@@ -1,133 +1,30 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Image, TextInput, TouchableOpacity } from 'react-native';
-import { ImageBackground } from 'react-native-web';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import log from './Component/log';
+import sign from './Component/sign';
+
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      
-        <ImageBackground source={{ uri: 'https://www.wallpapers13.com/wp-content/uploads/2018/04/Pink-Lily-Flower-with-Beautiful-Pink-Color-Photo-Wallpaper-HD-3840x2160-1600x1200.jpg' }}
-
-          resizeMode="cover" style={styles.image}
-       
-        >
-
-          <View style={{flex:1, backgroundColor: 'rgba(0,0,0,.6)',justifyContent: 'center', alignItems: 'center' }}>
-            <Text style={styles.TxtWeight}>Welcome Back</Text>
-
-            <View style={{ justifyContent: 'center', alignItems: 'center' }}>
-              <TextInput style={styles.input}
-                placeholder="Username"
-                placeholderTextColor="rgb(50,50,50)"
-              />
-              <TextInput
-                style={styles.input}
-                placeholder="Password"
-                placeholderTextColor="rgb(50,50,50)"
-              />
-            </View>
-
-            <TouchableOpacity style={{ justifyContent: 'center', alignItems: 'center' }} >
-              <Text style={styles.lgBtn}>Login</Text>
-            </TouchableOpacity>
-            <TouchableOpacity  >
-              <Text style={styles.Design1}>Forgot Password</Text>
-            </TouchableOpacity>
-
-            <View style={{ flexDirection: 'row', textAlign: 'center', justifyContent: 'center' }}>
-
-              <Text style={styles.Design1}>No account ?</Text>
-              <TouchableOpacity  >
-                <Text style={styles.Design}>Register</Text>
-              </TouchableOpacity>
-            </View>
-          </View>
-
-        </ImageBackground >
-
-      </View>
-    
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="log" component={log} option={{ headershown: false }} />
+        <Stack.Screen name="sign" component={sign}  />
+        
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    //backgroundColor: '#fff',
-    //alignItems: 'center',
-    //justifyContent: 'center',
-
-
-  },
-  lgBtn: {
-    height: 40,
-    width: 300,
-    color: 'white',
-    fontWeight: 'bold',
-    backgroundColor: '#db00b6',
-    borderRadius: 10,
-    //borderColor: 'white',
-    textAlign: 'center',
-    textAlignVertical:"center",
-    //borderWidth: 2,
-    marginTop: 20,
-    paddingHorizontal: 100,
-    paddingVertical: 10,
-    justifyContent:'center',
-    alignContent:'center'
-    
-  },
-  input: {
-    margin: 5,
-    height: 40,
-    width: 300,
-    backgroundColor: 'white',
-    borderRadius: 10,
-    paddingHorizontal: 15,
-    paddingVertical: 10,
-
-  },
-  TxtWeight: {
-    
-    fontSize: 30,
-    paddingBottom: 50,
-    color: '#db00b6',
+    backgroundColor: 'black',
     alignItems: 'center',
-    textAlign: 'center',
-    fontFamily: 'Helvetica',
-  
-
-  },
-  image: {
-    flex: 1,
-    justifyContent: "center",
-    backgroundColor: 'rgba(0,0,0,.6)'
-
-
-
-  },
-  Design: {
-    color: 'white',
-    fontSize: 12,
-    marginRight: 5,
-    marginTop: 51,
-    fontFamily: 'Helvetica',
-    textAlign: 'center',
-    justifyContent: 'center',
-    letterSpacing: 1,
-
-  },
-  Design1: {
-    color: '#db00b6',
-    fontSize: 15,
-    fontFamily: 'arial',
-    marginRight: 5,
-    marginTop: 50,
-    textAlign: 'center',
     justifyContent: 'center',
   },
-
-
-
 });
-
---
+++ Component/sign.js
@@ -0,0 +1,162 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View, Image, ScrollView, TextInput, TouchableOpacity } from 'react-native';
+import { ImageBackground } from 'react-native-web';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
+import { MaterialIcons } from '@expo/vector-icons';
+import { Feather } from '@expo/vector-icons';
+import { AntDesign } from '@expo/vector-icons';
+
+
+const sign = ({ navigation }) => {
+  return (
+    <View style={styles.container}>
+
+      <View style={{justifyContent:"center",alignContent:'center',alignItems:'flex-end'}}>
+      <MaterialIcons name="person" size={24} justifyContent='end' color="blue" margin={30} paddingHorizontal={30} paddingBottom={50} />
+      </View>
+
+      <ImageBackground source={{ uri: 'https://www.wallpapers13.com/wp-content/uploads/2018/04/Pink-Lily-Flower-with-Beautiful-Pink-Color-Photo-Wallpaper-HD-3840x2160-1600x1200.jpg' }}
+        resizeMode="cover" style={styles.image}
+      >
+        <View style={styles.back}>
+          <Text style={styles.TxtWeight}>Points</Text>
+        </View>
+      </ImageBackground >
+
+      <Text style={styles.texx}>History</Text>
+
+      <View style={{ justifyContent: 'center', alignItems: 'center', justifyContent: 'space-between' }}>
+
+        <TouchableOpacity style={styles.desig}>
+         
+          <AntDesign name="upload" size={24} color="blue" />
+          <Text> gh </Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity style={styles.desig} >
+          <AntDesign name="download" size={24} color="blue" paddingHorizontal={20} paddingVertical={10} />
+        </TouchableOpacity>
+
+        <TouchableOpacity style={styles.desig}>
+          <AntDesign name="upload" size={24} color="blue" />
+        </TouchableOpacity>
+
+        <TouchableOpacity style={styles.desig}>
+          <AntDesign name="download" size={24} color="blue" paddingHorizontal={20} paddingVertical={10} />
+          <Text style={{ alignContent: 'center', justifyContent: 'center', justifyContent: "space-around",alignItems:'center', paddingHorizontal:"15"}}></Text>
+        </TouchableOpacity>
+
+        <MaterialCommunityIcons name="qrcode-scan" size={24} color="black" paddingHorizontal="15" alignContent="center" justifyContent="center" />
+        
+      </View>
+    </View>
+
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#f8c8dc',
+    //alignItems: 'center',
+    //justifyContent: 'center',
+
+
+  },
+  lgBtn: {
+    height: 50,
+    width: 400,
+    color: 'pink',
+    fontWeight: 'bold',
+    backgroundColor: 'white',
+    borderRadius: 10,
+    //borderColor: 'white',
+    textAlign: 'center',
+    textAlignVertical: "center",
+    //borderWidth: 2,
+    marginTop: 20,
+    paddingHorizontal: 100,
+    paddingVertical: 10,
+    justifyContent: 'center',
+    alignContent: 'center'
+
+  },
+  input: {
+    margin: 5,
+    height: 50,
+    width: 400,
+    backgroundColor: 'white',
+    borderRadius: 10,
+    paddingHorizontal: 15,
+    paddingVertical: 10,
+
+  },
+  TxtWeight: {
+
+    fontSize: 30,
+    paddingBottom: 50,
+    color: 'blue',
+    alignItems: 'center',
+    textAlign: 'center',
+    fontFamily: 'Helvetica',
+
+
+  },
+  image: {
+    flex: 1,
+    justifyContent: "center",
+    backgroundColor: 'rgba(0,0,0,.6)',
+    //height :200
+
+
+
+  },
+  Design: {
+    color: 'white',
+    fontSize: 12,
+    marginRight: 5,
+    marginTop: 51,
+    fontFamily: 'Helvetica',
+    textAlign: 'center',
+    justifyContent: 'center',
+    letterSpacing: 1,
+
+  },
+  Design1: {
+    color: '#db00b6',
+    fontSize: 15,
+    fontFamily: 'arial',
+    marginRight: 5,
+    marginTop: 50,
+    textAlign: 'center',
+    justifyContent: 'center',
+  },
+  texx: {
+    margin: 10,
+    paddingHorizontal: 20,
+    fontSize: 18,
+    fontWeight: 'bold',
+    alignContent: 'center',
+    alignItems: 'center',
+    justifyContent: 'center',
+    color: 'blue'
+  },
+  back: {
+    flex: 1,
+    backgroundColor: 'rgba(0,0,0,.6)',
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: '100'
+  },
+  desig: {
+    margin: 10,
+    backgroundColor: 'white',
+    width: 400,
+    height: 50,
+    borderRadius: 10,
+    flexDirection:'row',
+    justifyContent:'space-between'
+  },
+
+});
+export default sign
